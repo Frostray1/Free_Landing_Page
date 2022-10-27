70f214ca276acc53d3e784de0cd9d62cdadb2651
Artem <a.bychkoff1@yandex.ru>
2022-10-27 16:27:47 +0300

    Добавлено меню в версию для планшета

--- js/menu.js
+++ js/menu.js
@@ -2,5 +2,6 @@ $(document).ready(function() {
     $('.menu-burger__header').click(function(){
         $('.menu-burger__header').toggleClass('open-menu');
         $('.menu').toggleClass('open-menu');
+        $('.profile__lang').toggleClass('profile__lang--open-menu');
     });
 });
